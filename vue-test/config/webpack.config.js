7035fedd26901e2c6e31b419322f8f8e8d299f86
yiboYin <1535213303@qq.com>
2018-08-07 17:55:38 +0800

    vue-webpack 配置试验项目

---
+++ vue-test/config/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin
+    ]
+}
